1400d1800aa39626ca364a7498bbf40efbc70804
dianlujitao <dianlujitao@lineageos.org>
2019-10-18 11:05:40 +0800

    sm8150-common: Enable QC framework value-adds
    Change-Id: I1987d3509b3db7cd4204537a6f2dc2ff4c611278

--- common.mk
+++ common.mk
@@ -382,6 +382,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/etc/permissions/privapp-permissions-oem-system.xml:system/etc/permissions/privapp-permissions-oem-system.xml
 
+# Qualcomm
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.va_aosp.support=1
+
+PRODUCT_ODM_PROPERTIES += \
+    ro.vendor.qti.va_odm.support=1
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
