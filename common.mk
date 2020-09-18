5be1e2d6995e7a304260eb09de199bc8a315b3b2
Anay Wadhera <awadhera@berkeley.edu>
2020-09-17 22:09:39 -0700

    sm8150-common: Inherit power product makefile
    * this will automatically include the correct powerhint and impl, so why not?
    
    Change-Id: I6fbfeabfc9bcb8c207d39aaf457cfc52c7bca928

--- common.mk
+++ common.mk
@@ -376,8 +376,7 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+$(call inherit-product, vendor/qcom/opensource/power/power-vendor-product.mk)
 
 # Protobuf
 PRODUCT_PACKAGES += \
