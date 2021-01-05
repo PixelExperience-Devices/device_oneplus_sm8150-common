dc4a2a69a03ffb0db00a0cb8e2eca68a0b2b485a
LuK1337 <priv.luk@gmail.com>
2021-01-05 13:42:00 +0100

    sm8150-common: Switch to QTI bootctrl HAL
    Change-Id: Ie5079fd4c40cb1f7414f2cba9f98d350f03047d8

--- common.mk
+++ common.mk
@@ -106,9 +106,9 @@ PRODUCT_COPY_FILES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl:64 \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
     android.hardware.boot@1.0-service \
-    android.hardware.boot@1.0-impl.recovery \
     bootctrl.msmnile \
     bootctrl.msmnile.recovery
 
