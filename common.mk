e3eda294f83d0977973988ed15bf0b929c29d85b
baalajimaestro <me@baalajimaestro.me>
2021-01-29 12:33:05 +0530

    sm8150-common: Uprev soundtrigger to 2.3
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Change-Id: I690f262f625ae6d1cd19abd5afef541718a00d56

--- common.mk
+++ common.mk
@@ -107,7 +107,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.common@2.0-util \
     android.hardware.audio.common@5.0-util \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.3-impl \
+    android.hardware.soundtrigger@2.3-service \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
