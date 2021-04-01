7ae21919b26bdfbae83a4554a2d260bad80fcff2
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-04-01 22:16:16 +0530

    sm8150-common: Uprev radio to 1.5
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I8bfe34a4fcd19aa8b8a410049e5123a3e34a586b

--- common.mk
+++ common.mk
@@ -396,9 +396,9 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2 \
     android.hardware.radio@1.5.vendor \
-    android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
     libprotobuf-cpp-full \
     libril_shim \
     librmnetctl \
