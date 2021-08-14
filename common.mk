c94496525330ada09ab478f57e71d093b5c43543
dantmnf <dantmnf2@gmail.com>
2021-08-15 01:23:14 +0800

    sm8150-common: rework color management and display modes
    * Enable hwcomposer HAL to switch panel mode via sysfs.
    * Patch QDCM calibration data to include exactly one mode for each
      ColorGamut attribute.
      * We don't have good control over HAL color mode to SDM/QDCM mode
        mappings.
      * 2 of 3 QDCM data files in OOS doesn't load any calibration except
        for HDR. It does contains 3DLUTs for the panel, but OnePlus
        disabled them in favor of calibration in panel driver (controlled
        via DSC commands).
    * Disable LiveDisplay DisplayModes in favor of ColorDisplayManager as
      it offers better support for color management.
      * This hides extra panel modes from user, until we figure out how
        to override SDM mode decision in hwcomposer blob and have some
        new settings UI to configure mappings between HAL_COLOR_MODE_*
        and QDCM modes.
    
    Change-Id: I446402789f4cbb6515d2a1799be075cd0cf38dfc
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- common.mk
+++ common.mk
@@ -204,6 +204,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     gralloc.msmnile \
     hwcomposer.msmnile \
+    hwcomposer.oneplus_sm8150 \
     libdisplayconfig.qti \
     libdisplayconfig.qti.vendor \
     libqdMetaData \
