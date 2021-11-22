6376d40cd975bf375e6c6a08f466db49cb8e65c0
chandra <chandra.chaganti@hotmail.com>
2021-11-22 12:23:13 +0530

    sm8150-common: drop livedisplay

--- common.mk
+++ common.mk
@@ -325,10 +325,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.oneplus_msmnile
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
