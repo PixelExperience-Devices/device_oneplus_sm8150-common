7b119ea7a7cc2f7a955fe339bf27d7555d90f9cb
chandu078 <chandudyavanapelli03@gmail.com>
2022-01-02 04:51:34 +0000

    guacamoleb: rro_overlays: Add RRO for OnePlusCamera & OnePlusGallery
    Change-Id: I515c929c5c7f2b78ef8f758bb5423afd7cfb7d61

--- common.mk
+++ common.mk
@@ -392,6 +392,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     oneplus-fwk
 
+# OnePlus Apps
+PRODUCT_PACKAGES += \
+    OnePlusCameraOverlay \
+    OnePlusGalleryOverlay
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
