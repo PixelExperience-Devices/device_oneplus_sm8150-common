7325ec761dee66da04ed1c857b8857b82cabafe0
Bruno Martins <bgcngm@gmail.com>
2022-01-27 11:32:26 +0000

    sm8150-common: Build Goodix fingerprint HIDL interface from source
    Change-Id: I1a3ae9a726c29d813d8ea5a8ef8f68adf988d4ef

--- common.mk
+++ common.mk
@@ -255,7 +255,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.oneplus \
-    vendor.oneplus.hardware.display@1.0.vendor
+    vendor.oneplus.hardware.display@1.0.vendor \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 # GPS
 PRODUCT_COPY_FILES += \
