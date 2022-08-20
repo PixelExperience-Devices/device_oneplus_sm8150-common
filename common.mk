6eeb3de51c899acdee4a22bce2967c34a3ee7a1d
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2022-08-20 20:32:36 +0530

    sm8150-common: Build new OnePlus camera HALS

--- common.mk
+++ common.mk
@@ -166,7 +166,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
-    vendor.oneplus.camera.CameraHIDL@1.0.vendor \
+    vendor.oneplus.hardware.camera@1.0.vendor:64 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Common init scripts
