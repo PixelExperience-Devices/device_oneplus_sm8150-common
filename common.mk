371a0011ff71f958974a15bde192626a78c6ecfc
Rajan Palaniya <razorpaul@gmail.com>
2022-10-10 18:05:14 +0530

    sm8150-common: move to erofs from ext4
    * Make default BOARD_EROFS_COMPRESSOR to LZ4
    * Allow BOARD_DYNAMIC_PARTITIONS_FILESYSTEM to be overridable

--- common.mk
+++ common.mk
@@ -4,11 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BOARD_DYNAMIC_PARTITIONS_FILESYSTEM ?= erofs
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=$(BOARD_DYNAMIC_PARTITIONS_FILESYSTEM) \
     POSTINSTALL_OPTIONAL_system=true
 
 PRODUCT_PACKAGES += \
