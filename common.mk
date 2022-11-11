cc69deb0a2df3e1d4b5e5b86e25de85bb0af2318
Rajan Palaniya <razorpaul@gmail.com>
2022-11-11 19:05:15 +0530

    [PARTIAL]: Revert "sm8150-common: Drop LiveDisplay and Lineage Trust HAL"
    This reverts commit 1a9939bb257229d687a24541888a86ed6bbf8e56.

--- common.mk
+++ common.mk
@@ -242,6 +242,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.1-service.oneplus_msmnile
+
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
