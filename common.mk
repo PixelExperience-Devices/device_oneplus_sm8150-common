6f026d48465558d9a3e0c5d0c074e4bade06381f
Rajan Palaniya <razorpaul@gmail.com>
2023-06-04 11:49:12 +0530

    sm8150-common: import some perf configs from vendor
    Change-Id: I9317358aaea70aa2637e4c98d723bd26d5dec6ef

--- common.mk
+++ common.mk
@@ -320,6 +320,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
