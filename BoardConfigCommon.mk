hotdogg-dev/android_device_oneplus_sm8150-common

sm8150-common: Import common bdroid_buildcfg
Now that BTM_DEF_LOCAL_NAME is gone (migrated to a sysprop)
we can commonize the header.

Change-Id: I67da0b9f877da358f71c7afdca448fc8cc58773e
Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

BoardConfigCommon.mk
@@ -30,6 +30,9 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
 TARGET_BOOTLOADER_BOARD_NAME := msmnile
 TARGET_NO_BOOTLOADER := true
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(VENDOR_PATH)/bluetooth/include
+
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000

PixelExperience-Devices/device_oneplus_sm8150-common

BoardConfigCommon.mk
@@ -44,6 +44,9 @@ AUDIO_FEATURE_ENABLED_INSTANCE_ID := true
 AUDIO_FEATURE_ENABLED_PROXY_DEVICE := true
 USE_CUSTOM_AUDIO_POLICY := 1
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth/include
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := msmnile
 TARGET_NO_BOOTLOADER := true