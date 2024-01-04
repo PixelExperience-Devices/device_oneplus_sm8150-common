BlissRoms-Devices/android_device_oneplus_sm8150-common

sm8150-common: Add product audio props

BoardConfigCommon.mk
@@ -62,6 +62,7 @@ TARGET_USES_QCOM_BSP := true
 
 # Properties
 TARGET_ODM_PROP += $(VENDOR_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(VENDOR_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(VENDOR_PATH)/system.prop
 TARGET_VENDOR_PROP += $(VENDOR_PATH)/vendor.prop
 

PixelExperience-Devices/device_oneplus_sm8150-common

Change-Id: I29bdcda15942972d33595312c3656c86b021481b

BoardConfigCommon.mk
@@ -51,6 +51,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop